5fd17630a04930434b34130e50723e0fdc17832e
husen <husenmalik7@gmail.com>
2020-01-10 11:45:00 +0900

    modal biasa dari bawaan, sebelum install react-native-modal dari npm

--- src/screens/Engineer/EngineerListScreen.js
+++ src/screens/Engineer/EngineerListScreen.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button, TextInput, Image, FlatList } from 'react-native';
+import { StyleSheet, View, Text, Button, TextInput, Modal, TouchableHighlight,
+        Image, FlatList, Alert } from 'react-native';
 // import { Searchbar } from 'react-native-paper';
 import ActionButton from 'react-native-action-button';
 
 import axios from 'axios';
 
-const url = 'http://192.168.6.139:9000/'
+const url = 'http://192.168.6.140:9000/'
 
 class EngineerListScreen extends React.Component {
     state = {
-        engineerList: []
+        engineerList: [],
+        modalVisible: false
+    }
+
+    setModalVisible(visible) {
+      this.setState({modalVisible: visible})
     }
 
     componentDidMount() {
@@ -35,24 +41,22 @@ class EngineerListScreen extends React.Component {
     render() {
         return (
 
-            <View 
-            style={{
+          <View style={{
               flex: 1, 
               flexDirection: 'column',
               alignItems: 'stretch',
             }}>
 
               <View style={{
-                height: 50, 
-                alignItems: 'center',
-                flexDirection: 'row',
-                backgroundColor: 'powderblue', 
-                justifyContent: 'space-between',
-              }}>
+                  height: 50, 
+                  alignItems: 'center',
+                  flexDirection: 'row',
+                  backgroundColor: 'powderblue', 
+                  justifyContent: 'space-between',
+                }}>
                   <Image source={{uri: 'https://facebook.github.io/react/logo-og.png'}} style={{width: 20, height: 20}} />
                   <Text>2</Text>
                   <Image source={{uri: 'https://facebook.github.io/react/logo-og.png'}} style={{width: 20, height: 20}} />
-                  
               </View>
             
               <View style={{height: 50, backgroundColor: 'skyblue', alignItems: 'center',   justifyContent: 'center'}} >
@@ -61,27 +65,49 @@ class EngineerListScreen extends React.Component {
               </View>
 
               <View style={{flex: 1,  backgroundColor: 'steelblue'}} >
-            {console.log(this.state.engineerList, 'lopopoppopop')}
-
-
-                  <FlatList
-                    data={ this.state.engineerList }
-                    renderItem={({item}) => <Text>{item.name}</Text>}
-                    keyExtractor={(item, index) => index.toString() }
-                  />
+                {console.log(this.state.engineerList, 'lopopoppopop')}
+                <FlatList
+                  data={ this.state.engineerList }
+                  renderItem={({item}) => <Text>{item.name}</Text>}
+                  keyExtractor={(item, index) => index.toString() }
+                />
               </View>
 
 
               <ActionButton
-  buttonColor="rgba(231,76,60,1)"
-  onPress={() => { console.log("hi")}}
-/>
+                buttonColor="rgba(231,76,60,1)"
+                onPress={() => { console.log("hi")}}
+              />
+
+              <View style={{marginTop: 22}}>
+                <Modal
+                  animationType="slide"
+                  transparent={false}
+                  visible={this.state.modalVisible}
+                  onRequestClose={() => {Alert.alert('Modal has been closed')}}
+                >
+                  <View style={{marginTop: 22}}>
+                    <View>
+                      <Text>Wassup fellas</Text>
+                    
+                  
+
+                      <TouchableHighlight
+                        onPress={() => {this.setModalVisible(!this.state.modalVisible)} }
+                      >
+                      <Text>Hide momodl</Text>
+                      </TouchableHighlight>
+                    </View>
+                  </View>
+                </Modal>
 
 
-
-
-
-
+                <TouchableHighlight
+                  onPress={() => {this.setModalVisible(true)}}
+                >
+                  <Text>SSHOw modalas</Text>
+                </TouchableHighlight>
+              </View>
 
           </View>
         );
